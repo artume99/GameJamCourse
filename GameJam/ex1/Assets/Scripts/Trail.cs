08d01c17c97f0546e8c593a89f5eb3cfac95739e
artume99 <artume.pichersky@mail.huji.ac.il>
2021-10-26 14:11:28 +0300

    nothing to big

--- GameJam/ex1/Assets/Scripts/Trail.cs
+++ GameJam/ex1/Assets/Scripts/Trail.cs
@@ -1,8 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Trail : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject[] trails;
+
+    // private void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     for (int i = 0; i < trails.Length; i++)
+    //     {
+    //         trails[i].transform
+    //     }
+    // }
+    //
+    // private void OnTriggerExit2D(Collider2D other)
+    // {
+    //     for (int i = 0; i < trails.Length; i++)
+    //     {
+    //         trails[i].SetActive(false);
+    //     }
+    // }
 }
